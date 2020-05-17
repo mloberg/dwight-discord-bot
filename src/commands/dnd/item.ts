80585a080a87b58522ee41837048aac5b5db49dc
Matt Loberg <loberg.matt@gmail.com>
2020-05-17 14:33:51 -0500

    feat(item): allow filtering by item type

--- src/commands/dnd/item.ts
+++ src/commands/dnd/item.ts
@@ -1,4 +1,4 @@
-import { Command } from "discord.js-commando";
+import { Command, FriendlyError } from "discord.js-commando";
 
 import itemList from "../../data/items.json";
 import { rand } from "../../utils";
@@ -18,15 +18,31 @@ export default class Treasure extends Command {
                     key: "rarity",
                     prompt: "What item rarity to return?",
                     type: "string",
-                    validate: v => ["common", "uncommon", "rare", "very rare", "legendary", "varies"].includes(v.toLowerCase()),
+                    validate: v => ["common", "uncommon", "rare", "very rare", "legendary"].includes(v.toLowerCase()),
+                    parse: v => v.toLowerCase(),
+                },
+                {
+                    key: "type",
+                    prompt: "What type of item should I return?",
+                    type: "string",
+                    default: "",
+                    parse: v => v.toLowerCase() === "wondrous" ? "wondrous item" : v.toLowerCase(),
                 },
             ],
         });
     }
 
     async run(msg, args) {
-        const items = itemList.filter(i => i.rarity === args.rarity.toLowerCase());
+        let items = itemList.filter(i => i.rarity === args.rarity);
+
+        if (args.type) {
+            items = items.filter(i => i.type.toLowerCase() === args.type);
+        }
+
         const item = rand(items);
+        if (!item) {
+            throw new FriendlyError("I could not find an item with those parameters.");
+        }
 
         return msg.reply(item.item);
     }
