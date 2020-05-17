ce96fc86f5072b43537922da18d98c812e0d6082
Matt Loberg <loberg.matt@gmail.com>
2020-05-16 22:34:38 -0500

    style: fix formatting of crit type

--- src/types/crit.ts
+++ src/types/crit.ts
@@ -7,19 +7,19 @@ export default class CritType extends ArgumentType {
 
     validate(val, _, arg) {
         const int = Number(val.replace(/^!/, ""));
-		if (Number.isNaN(int)) {
+        if (Number.isNaN(int)) {
             return false;
         }
-		if (arg.oneOf && !arg.oneOf.includes(int)) {
-			return `Please enter one of the following options: ${arg.oneOf.map(opt => `\`${opt}\``).join(", ")}`;
-		}
-		if (arg.min !== null && typeof arg.min !== "undefined" && int < arg.min) {
-			return `Please enter a number above or exactly ${arg.min}.`;
-		}
-		if (arg.max !== null && typeof arg.max !== "undefined" && int > arg.max) {
-			return `Please enter a number below or exactly ${arg.max}.`;
-		}
-		return true;
+        if (arg.oneOf && !arg.oneOf.includes(int)) {
+            return `Please enter one of the following options: ${arg.oneOf.map(opt => `\`${opt}\``).join(", ")}`;
+        }
+        if (arg.min !== null && typeof arg.min !== "undefined" && int < arg.min) {
+            return `Please enter a number above or exactly ${arg.min}.`;
+        }
+        if (arg.max !== null && typeof arg.max !== "undefined" && int > arg.max) {
+            return `Please enter a number below or exactly ${arg.max}.`;
+        }
+        return true;
     }
 
     parse(val) {
