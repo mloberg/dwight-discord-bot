e90494ecca4491d2548ee4dfe74bcb60b9e98691
Matt Loberg <loberg.matt@gmail.com>
2020-08-13 15:37:58 -0500

    add help command

---
+++ src/commands/help.ts
@@ -0,0 +1,46 @@
+import { Collection, Message } from 'discord.js';
+
+import { Arguments, Command } from '../types';
+import { env } from '../utils';
+
+const prefix = env('BOT_PREFIX', '__');
+
+const command: Command = {
+    name: 'help',
+    description: 'Get help with commands',
+    alias: ['commands'],
+    async run(message: Message, args: Arguments, commands: Collection<string, Command>): Promise<Message> {
+        if (!args._.length) {
+            return message.reply(
+                [
+                    'Here is a list of available commands:',
+                    commands.map((c) => c.name).join(', '),
+                    `Get more details with "${prefix}help [command]"`,
+                ],
+                { split: true },
+            );
+        }
+
+        const name = args._[0].toString().toLowerCase();
+        const command = commands.get(name) || commands.find((c) => c.alias && c.alias.includes(name));
+
+        if (!command) {
+            return message.channel.send("That's not a valid command");
+        }
+
+        const help = [`**${command.name}**: ${command.description}`];
+        if (command.alias) {
+            help.push(`*aliases*: ${command.alias.join(', ')}`);
+        }
+        if (command.usage) {
+            help.push(`*usage*: \`${prefix}${command.name} ${command.usage}\``);
+        }
+        if (command.examples) {
+            help.push(`*examples*: ${command.examples.map((e) => `\`${prefix}${command.name} ${e}\``).join(', ')}`);
+        }
+
+        return message.channel.send(help, { split: true });
+    },
+};
+
+export default command;
