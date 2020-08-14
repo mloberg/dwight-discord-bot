6841d2c1b72804424c132a6231a21f2a75036280
Matt Loberg <loberg.matt@gmail.com>
2020-08-14 14:25:53 -0500

    simplify event command
    filtering is a little weird, so remove it for now

--- src/commands/event.ts
+++ src/commands/event.ts
@@ -1,75 +1,38 @@
 import { Message } from 'discord.js';
 
-import { Arguments, Command } from '../types';
+import { Command } from '../types';
 import { random } from '../utils';
 
-const events: { event: string; where?: string[]; when?: string[] }[] = [
-    {
-        event: 'A door opens',
-        where: ['inside', 'town', 'dungeon'],
-    },
-    {
-        event: 'A fire starts',
-    },
-    {
-        event: 'A meteor shoots across the sky',
-        where: ['outside'],
-        when: ['night'],
-    },
-    {
-        event: 'A monster appears',
-    },
-    {
-        event: 'A screech pierces the air',
-    },
-    {
-        event: 'A storm begins',
-        where: ['inside', 'outside', 'town'],
-    },
-    {
-        event: 'A strange star appears in the sky',
-        where: ['outside'],
-        when: ['night'],
-    },
-    { event: 'A strong gust of wind blows through' },
-    { event: 'A tremor shakes the ground' },
-    { event: 'Someone experiences déjà vu' },
-    { event: 'Someone gets angry' },
-    { event: 'Someone glimpses the future' },
-    { event: 'Someone has a sense of foreboding' },
-    { event: 'Someone has to go to the bathroom' },
-    { event: 'Something spills or falls to the ground' },
-    { event: "Something isn't where it's supposed to be" },
-    { event: 'The lights go out' },
-    { event: 'The sun comes out' },
-    { event: "There's a foul smell in the air" },
-    { event: 'Unexplained magic occurs' },
+export const events = [
+    'A door opens',
+    'A fire starts',
+    'A meteor shoots across the sky',
+    'A monster appears',
+    'A screech pierces the air',
+    'A storm begins',
+    'A strange star appears in the sky',
+    'A strong gust of wind blows through',
+    'A tremor shakes the ground',
+    'Someone experiences déjà vu',
+    'Someone gets angry',
+    'Someone glimpses the future',
+    'Someone has a sense of foreboding',
+    'Someone has to go to the bathroom',
+    'Something spills or falls to the ground',
+    "Something isn't where it's supposed to be",
+    'The lights go out',
+    'The sun comes out',
+    "There's a foul smell in the air",
+    'Unexplained magic occurs',
 ];
 
 const command: Command = {
     name: 'event',
     description: 'Trigger a random event',
-    usage: '[--where] WHERE [--when] WHEN',
-    examples: ['town', 'outside night', '--when night'],
-    async run(message: Message, args: Arguments): Promise<Message> {
-        const where = args.where || args._[0];
-        const when = args.when || args._[1];
-
-        let filtered = events;
-
-        if (where) {
-            filtered = filtered.filter((e) => !e.where || e.where.includes(where.toLowerCase()));
-        }
-
-        if (when) {
-            filtered = filtered.filter((e) => !e.when || e.when.includes(when.toLowerCase()));
-        }
-
-        const event = random(filtered);
-
+    async run(message: Message): Promise<Message> {
         await message.delete();
 
-        return event ? message.channel.send(event.event) : null;
+        return message.channel.send(random(events));
     },
 };
 
