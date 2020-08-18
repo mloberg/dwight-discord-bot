e590fd3282e189d09056c2649dd11e4e56999359
Matt Loberg <loberg.matt@gmail.com>
2020-08-18 17:47:36 -0500

    fix prune test

--- src/commands/prune.ts
+++ src/commands/prune.ts
@@ -1,13 +1,18 @@
 import { Message } from 'discord.js';
 
+import { FriendlyError } from '../error';
 import { Arguments, Command } from '../types';
 
 const command: Command = {
     name: 'prune',
     description: 'Prune messages from a channel',
     usage: 'COUNT',
-    async run({ channel }: Message, args: Arguments): Promise<void> {
-        await channel.bulkDelete(Number(args._[0]) + 1, true);
+    async run(message: Message, args: Arguments) {
+        if (message.channel.type === 'dm') {
+            throw new FriendlyError("I can't bulk delete in DMs.");
+        }
+
+        await message.channel.bulkDelete(Number(args._[0]) + 1, true);
     },
 };
 
